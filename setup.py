a55d242e1b3ae29c9a019d0a624ce2522afb41f8
AmirSalar Safaei Ghaderi <71232995+amirsalarsafaei@users.noreply.github.com>
2025-04-14 12:31:37 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Kenar',
-    version='0.7.0',
+    version='0.7.1',
     author='Kenar',
     author_email='kenar@divar.ir',
     description='facilitate using kenar divar APIs',
